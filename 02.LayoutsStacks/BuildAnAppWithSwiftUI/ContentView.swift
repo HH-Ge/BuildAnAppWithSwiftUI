e798b1c861eff235517b5c61e523f1ca91340663
18611536087 <hh680821@163.com>
2020-03-19 10:48:36 +0800

    02.LayoutsStacks added

---
+++ 02.LayoutsStacks/BuildAnAppWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildAnAppWithSwiftUI
+//
+//  Created by Howard Ge on 2020/3/19.
+//  Copyright © 2020 Howard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("02.2 演出开始了!").font(.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
