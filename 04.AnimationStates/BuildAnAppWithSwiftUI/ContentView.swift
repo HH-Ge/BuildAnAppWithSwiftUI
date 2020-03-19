991818937d32da6b65af567a4d4622e9912cd5bf
18611536087 <hh680821@163.com>
2020-03-20 02:52:26 +0800

    04.AnimationStates added
    Section 4  finished

--- 04.AnimationStates/BuildAnAppWithSwiftUI/ContentView.swift
+++ 04.AnimationStates/BuildAnAppWithSwiftUI/ContentView.swift
@@ -12,32 +12,40 @@ import SwiftUI
  */
 
 struct ContentView: View {
+    @State private var show = false     // 声明一个状态，简单的布尔值可以控制两个状态
     var body: some View {
         ZStack {                    // 注意栈中各个组件的顺序和实际显示的层级关系
             TitleView()             // 标题视图
-                .blur(radius: 20)   // 模糊（虚化）
+                .blur(radius: show ? 10 : 0)            //根据 show 的值确定模糊度
+                .animation(.default)
             BackCardView()          // 最下面一层
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))   //根据 show 的值确定背景色
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40)
-                .scaleEffect(0.9)               // 缩放效果
-                .rotationEffect(.degrees(10))   // 旋转
+                .offset(x: 0, y: show ? -400 : -40)             //根据 show 的值确定偏移量
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))        //根据 show 的值确定旋转角度
                 .rotation3DEffect(.degrees(10), axis: (x: 10, y: 0, z: 0))      // 三维旋转
-                .blendMode(.hardLight)      // 混合模式，高光
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))           // 定义动画的类型和持续时间
             BackCardView()          // 中间一层
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200 : -20)
                 .scaleEffect(0.95)
-                .rotationEffect(Angle.degrees(5))
+                .rotationEffect(Angle.degrees(show ? 0 : 5))    // 根据 show 的状态确定旋转角度
                 .rotation3DEffect(.degrees(5), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))   // 这个持续时间与前面不同，可以形成错落的效果
             CardView()              // 最上面一层
                 .blendMode(.hardLight)
+                .onTapGesture {                         // 定义响应的动作
+                    self.show.toggle()                  // 切换 show 的状态
+            }
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
         }
     }
 }
